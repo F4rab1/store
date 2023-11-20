867d058b0ad2217af34786e31e368befe67e35f3
F4rab1 <issafarabi2022@gmail.com>
2023-11-20 23:41:50 +0600

    Admin page practicing

--- playground/views.py
+++ playground/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.db.models import Value, F, Func, Count
 from django.db.models.functions import Concat
 from django.db import transaction
-from store.models import Customer, Product, Collection
+from store.models import Customer, Product, Collection, Address
 from tags.models import TaggedItem
 
 
--- store/admin.py
+++ store/admin.py
@@ -1,13 +1,61 @@
+from typing import Any
 from django.contrib import admin
 from . import models
+from django.db.models import Count
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
 
-admin.site.register(models.Collection)
+@admin.register(models.Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['street', 'city']
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'product_count']
+
+    @admin.display(ordering='product_count')
+    def product_count(self, collection):
+        url = (
+            reverse('admin:store_product_changelist') 
+            + '?' 
+            + urlencode({
+                'collection__id': str(collection.id)
+            }))
+        return format_html('<a href="{}">{}</a>', url, collection.product_count)
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            product_count=Count('product')
+        )
+    
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 5
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
+    
+    
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['placed_at', 'payment_status', 'customer_fullname']
+    list_per_page = 10
+    list_select_related = ['customer']
+
+    def customer_fullname(self, order):
+        return order.customer.first_name + ' ' + order.customer.last_name
+    
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
